804c4286b6d14028023dc9f485f107b986d283fe
Karma-234 <busari1kamal@gmail.com>
2023-11-20 10:58:52 -0800

    WIP: Reslved merge conflict

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pomorodo_watch/main.dart';
+import 'package:pomorodo_watch/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
