5f0c8f93bacbf5633323e17a573bbf6615749b57
Karma-234 <busari1kamal@gmail.com>
2023-12-06 01:46:19 -0800

    Added color utils

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,60 @@
+import 'dart:ui';
+
+class AppNeutralColor {
+  static const Color text = Color(0xFF0D1C36);
+  static const Color textSubdued = Color(0xFF515772);
+  static const Color textDisabled = Color(0xFFB2B6CB);
+  static const Color textOn = Color(0xFFFFFFFF);
+  static const Color backgroundDefault = Color(0xFFF5F5F5);
+  static const Color backgroundSubdued = Color(0xFFF7F7F7);
+  static const Color backgroundXSubdued = Color(0xFFFAFAFA);
+  static const Color backgroundStrong = Color(0xFF060C1D);
+  static const Color secondaryButtonDefault = Color(0xFFEDEFF5);
+  static const Color border = Color(0xFFE6E8F0);
+  static const Color iconActive = Color(0xFF0D1C36);
+  static const Color iconDefault = Color(0xFF515772);
+  static const Color iconDisabled = Color(0xFFB2B6CB);
+}
+
+class AppPrimaryColor {
+  static const Color text = Color(0xFF062C30);
+  static const Color backgroundStrong = Color(0xFF062C30);
+  static const Color backgroundDefault = Color(0xFFF5F5F5);
+  static const Color border = Color(0xFF062C30);
+  static const Color icon = Color(0xFF062C30);
+  static const Color buttonDefault = Color(0xFF062C30);
+  static const Color buttonHover = Color(0xFF1F3E42);
+  static const Color buttonPressed = Color(0xFF042528);
+}
+
+class AppSuccessColor {
+  static const Color text = Color(0xFF2D765A);
+  static const Color backgroundStrong = Color(0xFF2D765A);
+  static const Color backgroundDefault = Color(0xFFF5FBF8);
+  static const Color border = Color(0xFF2D765A);
+  static const Color icon = Color(0xFF2D765A);
+}
+
+class AppWarningColor {
+  static const Color text = Color(0xFF66460D);
+  static const Color backgroundStrong = Color(0xFF66460D);
+  static const Color backgroundDefault = Color(0xFFFFFAF1);
+  static const Color border = Color(0xFF66460D);
+  static const Color icon = Color(0xFF66460D);
+}
+
+class AppDangerColor {
+  static const Color text = Color(0xFFAC2323);
+  static const Color backgroundStrong = Color(0xFFAC2323);
+  static const Color backgroundDefault = Color(0xFFFDF4F4);
+  static const Color border = Color(0xFFAC2323);
+  static const Color icon = Color(0xFFAC2323);
+}
+
+class AppTertiaryColor {
+  static const Color orange = Color(0xFFDE7548);
+  static const Color blue = Color(0xFF39A1EA);
+  static const Color green = Color(0xFF19A589);
+  static const Color red = Color(0xFFE06767);
+  static const Color gold = Color(0xFFE2D784);
+}
